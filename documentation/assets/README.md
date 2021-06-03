800b7930449803ed8b3f8a49120ae781b93b9022
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-06-03 15:21:18 +0100

    Restructure and rename to autoreduce_scripts

--- documentation/assets/README.md
+++
@@ -1,3 +0,0 @@
-As of this writing:
-
-* Autoreduction_main_components.png in main_components used in main README.md for repo
--- setup.py
+++ setup.py
@@ -7,17 +7,15 @@ for more details
 """
 from setuptools import setup, find_packages
 
-setup(name="autoreduce_qp",
-      version="22.0.0.dev",
-      description="ISIS Autoreduction queue processor",
-      author="ISIS Autoreduction Team",
-      url="https://github.com/ISISScientificComputing/autoreduce/",
-      install_requires=[
-          "autoreduce_utils==0.1.3", "autoreduce_db==0.1.4", "chardet==3.0.4", "Django==3.2.2", "fire==0.4.0",
-          "plotly==4.14.3", "stomp.py==6.1.0"
-      ],
-      packages=find_packages(),
-      entry_points={"console_scripts": ["autoreduce-qp-start = autoreduce_qp.queue_processor.queue_listener:main"]},
-      classifiers=[
-          "Programming Language :: Python :: 3 :: Only",
-      ])
+setup(
+    name="autoreduce_scripts",
+    version="22.0.0.dev",
+    description="ISIS Autoreduction queue processor",
+    author="ISIS Autoreduction Team",
+    url="https://github.com/ISISScientificComputing/autoreduce/",
+    install_requires=["autoreduce_qp"],
+    packages=find_packages(),
+    #   entry_points={"console_scripts": ["autoreduce-qp-start = autoreduce_qp.queue_processor.queue_listener:main"]},
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+    ])
