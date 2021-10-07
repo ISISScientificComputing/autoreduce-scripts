165552500773c2f03384d271ee90c265696ed0d0
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-07 09:12:32 +0100

    Fix inaccurate latest version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "django==3.2.8",
         "fire==0.4.0",
         "h5py==2.10.0",  # for reading the RB number from the datafile
-        "GitPython==3.1.25"  # for backup_reduction_scripts.py
+        "GitPython==3.1.24"  # for backup_reduction_scripts.py
     ],
     packages=find_packages(),
     entry_points={
