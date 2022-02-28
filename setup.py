5b5bc40af9170e1045b9009ea88c66e07ff685f5
James Brookes <james.brookes@stfc.ac.uk>
2022-02-28 21:14:23 +0000

    Increment versions

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="autoreduce_scripts",
-    version="22.0.0.dev39",
+    version="22.0.0.dev40",
     description="ISIS Autoreduce helper scripts",
     author="ISIS Autoreduction Team",
     url="https://github.com/autoreduction/autoreduce-scripts/",
     install_requires=[
-        "autoreduce_db==22.0.0.dev32",
-        "autoreduce_utils==22.0.0.dev18",
+        "autoreduce_db==22.0.0.dev33",
+        "autoreduce_utils==22.0.0.dev19",
         "django",  # will be matched with requirement in autoreduce_db
         "fire==0.4.0",
         "h5py<=3.6.0",  # for reading the RB number from the datafile
