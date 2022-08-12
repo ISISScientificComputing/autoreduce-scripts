96e1018e37a75038a9732e77b8b17fa78f591bc6
James Brookes <james.brookes@stfc.ac.uk>
2022-08-12 14:12:24 +0100

    Switch to tox (AR-1667) (#99)
    * Initial cimmit for Tox
    
    * Add parameterized for dev deps
    
    * Remove references to ActiveMQ
    
    * Ignore pylint warning
    
    * Fix yapf issue

--- autoreduce_scripts/nagios_checks/autoreduce_settings.py
+++ autoreduce_scripts/nagios_checks/autoreduce_settings.py
@@ -7,26 +7,13 @@
 """
 Settings for Nagios checks
 """
-from requests.auth import HTTPBasicAuth
-
-# ActiveMQ
-ACTIVEMQ = {
-    "host": "reducequeue",
-    "queues": ["ReductionPending", "ReductionError"],
-    "username": "YOUR-ACTIVEMQ-USERNAME",
-    "password": "YOUR-ACTIVEMQ-PASSWORD",
-    "api-path": ":8161/api/jolokia/read/org.apache.activemq:type=Broker,"
-    "brokerName=localhost,destinationType=Queue"
-}
+from autoreduce_utils.credentials import DB_CREDENTIALS
 
 MYSQL = {
-    "host": "reducequeue",
-    "username": "YOUR-MYSQL-USERNAME",
-    "password": "YOUR-MYSQL-USERNAME",
+    "host": DB_CREDENTIALS.host,
+    "username": DB_CREDENTIALS.username,
+    "password": DB_CREDENTIALS.password,
     "db": "autoreduction"
 }
 
 ISIS_MOUNT = 'Z:\\'
-
-ACTIVEMQ_URL = "http://" + ACTIVEMQ['host'] + ACTIVEMQ['api-path']
-ACTIVEMQ_AUTH = HTTPBasicAuth(ACTIVEMQ['username'], ACTIVEMQ['password'])
